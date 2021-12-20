9a5ce57df7ba9998c5f8178456db7a5e5eabd48d
Lucky <1446408517@qq.com>
2021-12-20 15:23:31 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "UserInterface.h"
+
+using namespace std;
+
+int main() {
+  //Instantiate a UserInterface object and invoke the interface
+  UserInterface interface;
+  interface.handleInput();
+  cout << endl;
+  system("pause");
+  return 0;
+}
